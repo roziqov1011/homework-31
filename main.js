4db0ee358ba3e3039e7330d126fb4705ba9bd20c
roziqov1011 <roziqov1011@gmail.com>
2021-09-16 16:26:09 +0500

    initial commit

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 function add(){
     var elage = document.getElementById('age').value;
-
-    if (elage < 56){
-        document.getElementById('endage').textContent = "siz oqishga kira olmadingiz";
+    var elCheck = document.getElementById('check');
+    if (elCheck.checked){
+        document.getElementById('endage').textContent = "siz karupsiya orqali kirdingiz !!!";
     }
-    if (elage >= 56){
-        document.getElementById('endage').textContent = "siz super kantrakt asosida o'qishga qabul qilindingiz";
+    else if (elage >= 189){
+        document.getElementById('endage').textContent = "siz tanka surgansiz max balldan oshib ketti😎";
     }
-    if (elage >= 80){
+    else if (elage >= 120){
+        document.getElementById('endage').textContent = "siz bujet asosida o'qishga qabul qilindingiz";
+    }
+    else if (elage >= 80){
         document.getElementById('endage').textContent = "siz kantrakt asosida o'qishga qabul qilindingiz";
     }
-    if (elage >= 120){
-        document.getElementById('endage').textContent = "siz bujet asosida o'qishga qabul qilindingiz";
-    }if (elage >= 189){
-        document.getElementById('endage').textContent = "siz tanka surgansiz max balldan oshib ketti😎";
+    else if (elage >= 56){
+        document.getElementById('endage').textContent = "siz super kantrakt asosida o'qishga qabul qilindingiz";
+    }
+    else if (elage < 56){
+        document.getElementById('endage').textContent = "siz oqishga kira olmadingiz";
     }
 }
